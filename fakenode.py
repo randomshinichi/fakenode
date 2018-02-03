9f43f8fb4e7b0e972ef7b202a73773b5bd23d28b
Andrew Chiw <randomshinichi4869@gmail.com>
2018-02-03 15:40:35 +0800

    JSON-RPC interface around miniQRL

--- fakenode.py
+++ fakenode.py
@@ -1,75 +1,89 @@
-from .miniqrl import *
-from flask import Flask, Response
+from miniqrl import *
+from flask import Flask, Response, request
 from jsonrpc.backend.flask import api
 from jsonrpc.exceptions import JSONRPCDispatchException
 
 
 @api.dispatcher.add_method
 def getlastblockheader():
-    pass
+    resp = s.getlastblockheader()
+    return resp
 
 
 @api.dispatcher.add_method
 def getblocktemplate(wallet_address):
-    pass
+    try:
+        blob, difficulty = s.getblocktemplate(wallet_address)
+    except Exception:
+        raise JSONRPCDispatchException(code=-9, message="Core is busy")
+
+    resp = {
+        "blocktemplate_blob": blob,
+        "difficulty": difficulty
+    }
+    return resp
 
 
 @api.dispatcher.add_method
-def submitblock(raw_block_merkle_root_w_nonce):
-    pass
+def submitblock(binary_blob):
+    msg = s.submitblock(binary_blob)
+
+    if msg == "OK":
+        return {"status": msg}
+    else:
+        raise Exception(msg)
 
 
-fake_node = State()
+s = State()
 
 app = Flask(__name__)
 app.add_url_rule('/json_rpc', 'api', api.as_view(), methods=['POST'])
 
 
-@app.route('/create_block')
-def create_block():
-    fake_node.create_block()
-    resp = "fake_node.blocks length: %s\n" % len(fake_node.blocks)
+@app.route('/add_block')
+def add_block():
+    s.add_block()
+    resp = "s.blocks length: %s\n" % len(s.blocks)
     return Response(response=resp, status=200, mimetype='text/plain')
 
 
-@app.route('/synced')
-def set_fake_node_synced():
-    fake_node.synced = True
-    resp = "fake_node.synced: %s\n" % fake_node.synced
+@app.route('/txpool/add')
+def add_txpool():
+    s.fill_txpool()
+    resp = "s.txpool length: %s\n" % len(s.txpool)
     return Response(response=resp, status=200, mimetype='text/plain')
 
 
-@app.route('/unsynced')
-def set_fake_node_unsynced():
-    fake_node.synced = False
-    resp = "fake_node.synced: %s\n" % fake_node.synced
+@app.route('/txpool/empty')
+def empty_txpool():
+    s.empty_txpool()
+    resp = "s.txpool length: %s\n" % len(s.txpool)
     return Response(response=resp, status=200, mimetype='text/plain')
 
 
-@app.route('/accept_submitted_blocks')
-def set_node_accept_submitted_blocks():
-    fake_node.accept_submitted_blocks = True
-    resp = "fake_node.accept_submitted_blocks: %s\n" % fake_node.accept_submitted_blocks
-    return Response(response=resp, status=200, mimetype='text/plain')
+@app.route('/params', methods=['GET', 'POST'])
+def set_params():
+    def true_or_false(i):
+        return True if i.lower() == 'true' else False
+
+    if request.method == 'POST':
+        if request.form.get("synced"):
+            s.synced = true_or_false(request.form.get("synced"))
+        if request.form.get("difficulty"):
+            s.difficulty = request.form.get("difficulty")
+        if request.form.get("did_difficulty_calculation_pass"):
+            s.did_difficulty_calculation_pass = true_or_false(request.form.get("did_difficulty_calculation_pass"))
+
+    resp = "s.synced = {}\ns.difficulty = {}\ns.did_difficulty_calculation_pass = {}\n".format(
+        s.synced, s.difficulty, s.did_difficulty_calculation_pass)
+
+    return Response(response=resp, status=200, mimetype='application/json')
 
 
-@app.route('/reject_submitted_blocks')
-def set_node_reject_submitted_blocks():
-    fake_node.accept_submitted_blocks = False
-    resp = "fake_node.accept_submitted_blocks: %s\n" % fake_node.accept_submitted_blocks
-    return Response(response=resp, status=200, mimetype='text/plain')
-
-
-@app.route('/inspect_state')
+@app.route('/ipdb')
 def inspect_state():
     import ipdb
     ipdb.set_trace()
-    return Response(response="done", status=200, mimetype='text/plain')
-
-
-@app.route('/reset_state')
-def reset_state():
-    fake_node.blocks = []
     return Response(response="done", status=200, mimetype='text/plain')
 
 
