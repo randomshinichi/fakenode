6a13396ef258011f90fcab8ecdf4fa5f5c5e4e97
Andrew Chiw <randomshinichi4869@gmail.com>
2018-02-05 18:00:57 +0800

    Testqrl: did_difficulty_calculation_pass

--- testqrl.py
+++ testqrl.py
@@ -15,3 +15,16 @@ blob = s.getblocktemplate("Qaddr")[0]
 s.add_block()
 s.add_block()
 print(s.submitblock("12345678" + blob))
+
+print("Mining - difficulty calculation did not pass")
+s = State()
+s.did_difficulty_calculation_pass = False
+blob = s.getblocktemplate("Qaddr")[0]
+print(s.submitblock("12345678" + blob))
+
+
+print("Mining - different blob")
+s = State()
+s.did_difficulty_calculation_pass = False
+blob = s.getblocktemplate("Qaddr")[0]
+print(s.submitblock("12345678" + blob + 'a'))
