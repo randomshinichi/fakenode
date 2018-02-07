dfdf18c6395d2f56bd8dcba81d8a8e5a00826a12
Andrew Chiw <randomshinichi4869@gmail.com>
2018-02-07 21:42:13 +0800

    All 0s to see very clearly where the miner rolls the nonce

--- fakenode.py
+++ fakenode.py
@@ -3,32 +3,31 @@ from flask import Flask
 from jsonrpc.backend.flask import api
 from jsonrpc.exceptions import JSONRPCDispatchException
 
-
-app = Flask(__name__)
-app.add_url_rule('/json_rpc', 'api', api.as_view(), methods=['POST'])
+someblockheader = {
+    "block_header": {
+        "depth": 0,
+        "difficulty": 10,
+        "hash": 'deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef',
+        "height": 1,
+        "nonce": 00000000,
+        "num_txes": 1,
+        "orphan_status": False,
+        "prev_hash": '0000000000000000000000000000000000000000000000000000000000000000',
+        "reward": 50,
+        "timestamp": 1511404873
+    },
+    "status": 'OK'
+}
 
 
 @api.dispatcher.add_method
-def getlastblockheader(*args, **kwargs):
-    lastblockheader = {
-        "block_header": {
-            "block_size": 251735,
-            "depth": 0,
-            "difficulty": 10,  # 34407423123
-            "hash": '967a4b987cfd73a3707a0347664307700e7c989aa644ba380e3b44d980c6e47a',
-            "height": 1,  # 1448983
-            "major_version": 6,
-            "minor_version": 6,
-            "nonce": 25167459,
-            "num_txes": 19,
-            "orphan_status": False,
-            "prev_hash": '68271d9edc7cc560dbb2adbc00f9dbe2ae67a630cc7146556a0c736d0316312c',
-            "reward": 6556365000456,
-            "timestamp": 1511404873
-        },
-        "status": 'OK'
-    }
-    return lastblockheader
+def getlastblockheader():
+    return someblockheader
+
+
+@api.dispatcher.add_method
+def getblockheaderbyheight(height):
+    return someblockheader
 
 
 @api.dispatcher.add_method
@@ -37,7 +36,7 @@ def getblocktemplate_coreisbusy(*args, **kwargs):
 
 
 @api.dispatcher.add_method
-def getblocktemplate(*args, **kwargs):
+def getblocktemplate(wallet_address):
     with open('block.json') as f:
         block = json.load(f)
         print("loaded JSON file")
@@ -45,5 +44,11 @@ def getblocktemplate(*args, **kwargs):
 
 
 @api.dispatcher.add_method
-def submitblock(*args, **kwargs):
+def submitblock(blob):
+    print("submitblock:", blob)
     return {"status": "OK"}
+
+
+app = Flask(__name__)
+app.add_url_rule('/json_rpc', 'api', api.as_view(), methods=['POST'])
+app.run(port=18081)
